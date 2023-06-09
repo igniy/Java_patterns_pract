a3a4c54ee7326495791ed56588590d36eb5ff53e
igniy <69981971+igniy@users.noreply.github.com>
2023-06-10 01:17:15 +0300

    Add files via upload

---
+++ pract11/src/test/java/ru/Kvashnin/pract11/Pract11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Kvashnin.pract11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
